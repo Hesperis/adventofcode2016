0241adad60e020362da8046951e3c6c4c68a6ffd
Karin Obermüller <karin.obermuller@meltwater.com>
2016-12-04 12:28:01 +0100

    refactored third day some

--- src/adventofcode/thirdday/Third.kt
+++ src/adventofcode/thirdday/Third.kt
@@ -13,45 +13,49 @@ class Third {
     }
 }
 
-private var allTriads :MutableList<List<Int>> = mutableListOf()
-
 fun first() {
     val lines = File("input/Day3Input.txt").readLines()
-    var count = 0
-    lines.forEach { if(tryIfValidTriangle(it.trim().replace("\\s+".toRegex(),","))) count++ }
+    val count = lines
+            .map {
+                it
+                    .trim()
+                    .split("\\s+".toRegex())
+                    .map(String::toInt)
+                    .sorted()
+            }
+            .filter { it[2] < it[0] + it[1] }.count()
     println("Number of valid triangles is $count")
 }
 
-fun tryIfValidTriangle(triangleString: String): Boolean {
-    val triangle = triangleString.split(",").map(String::toInt).sorted()
-    return triangle.all { triangle[2] < triangle[1] + triangle[0] }
-}
 
 fun second() {
     val lines = File("input/Day3input.txt").readLines()
-    val trimmedLines = lines.map { it.trim().replace("\\s+".toRegex(),",")}
+
     val columns: List<MutableList<Int>> = listOf(mutableListOf(), mutableListOf(), mutableListOf())
 
-    trimmedLines.map { it.split(',') }
+    lines.map { it.trim().split("\\s+".toRegex()) }
             .forEach{
                 columns.get(0).add(it.get(0).toInt())
                 columns.get(1).add(it.get(1).toInt())
                 columns.get(2).add(it.get(2).toInt())
     }
-    columns.forEach(::splitByThree)
-    var count = 0
-    allTriads.forEach {
-        if(it[0] + it[1] > it[2]) count++
-    }
-    println(count)
+
+    val allTriples = columns
+        .flatten()
+        .splitByThree()
+        .map{ it.sorted() }
+
+    val count = allTriples.filter {
+        (it[0] + it[1] > it[2])
+    }.count()
+
+    println("Valid triangles are $count")
 }
 
-fun splitByThree(column: List<Int>){
-    var i = 0
-    while (i < column.size) {
-        val snippet = listOf<Int>(column.get(i), column.get(i+1), column.get(i+2))
-        allTriads.add(snippet.sorted())
-        i += 3
-    }
-
+fun <T> List<T>.splitByThree(): List<List<T>>{
+    return this
+            .withIndex()
+            .groupBy { v -> v.index / 3 }
+            .values
+            .map{ it.map {it -> it.value}}
 }

afa4bd894114f29646348bde7f3beeb4ed41d085
Karin Obermüller <karin.obermuller@meltwater.com>
2016-12-04 12:58:34 +0100

    Restructure and clearer var names for Fourth

--- src/adventofcode/fourthday/Fourth.kt
+++ src/adventofcode/fourthday/Fourth.kt
@@ -22,6 +22,16 @@ fun first() {
     println(count)
 }
 
+fun second() {
+    val lines = File("input/Day4Input.txt").readLines()
+    lines
+            .filter(::isValidRooms)
+            .map(::splitToWordsAndIndexModulator)
+            .map(::decrypt)
+            .filter { nameWithId -> "north" in nameWithId.first }
+            .map { nameWithId -> println("Room name is ${nameWithId.first}, sector id is ${nameWithId.second}") }
+}
+
 fun splitString(string: String): List<String> {
     return string
             .trim()
@@ -34,13 +44,13 @@ fun isValidRooms(string: String): Boolean {
     val hash = room[1].toCharArray().toList()
     val sortedchars = room[0]
             .toCharArray()
-            .filterNot { it == '-' || it.isDigit() }
-            .groupBy { it }
+            .filterNot { char -> char == '-' || char.isDigit() }
+            .groupBy { char -> char }
             .toSortedMap()
             .toList()
-            .sortedByDescending{it.second.size}
+            .sortedByDescending{charGroup -> charGroup.second.size}
             .subList(0,5)
-            .map { it.first }
+            .map { charGroup -> charGroup.first }
     return sortedchars == hash
 }
 
@@ -49,17 +59,6 @@ fun findSectorIds(string: String): Int {
             .toCharArray()
             .filter { it.isDigit() }
             .joinToString(separator = "").toInt()
-}
-
-
-fun second() {
-    val lines = File("input/Day4Input.txt").readLines()
-    lines
-            .filter(::isValidRooms)
-            .map(::splitToWordsAndIndexModulator)
-            .map(::decrypt)
-            .filter { "north" in it.first }
-            .map { println("Room name is ${it.first}, sector id is ${it.second}") }
 }
 
 fun splitToWordsAndIndexModulator(string: String): Pair<List<String>, Int> {
@@ -71,10 +70,10 @@ fun splitToWordsAndIndexModulator(string: String): Pair<List<String>, Int> {
 fun decrypt(room: Pair<List<String>, Int>): Pair<String, Int> {
     val string = room.first
             .map{
-                it
+                roomName -> roomName
                 .toCharArray()
                 .map {
-                    getCharAtIndex(getIndexforChar(it) + room.second)
+                    getCharAtIndex(getIndexForChar(it) + room.second)
                 }
                 .joinToString(separator = "")
             }.joinToString(separator = " ")
@@ -84,11 +83,11 @@ fun decrypt(room: Pair<List<String>, Int>): Pair<String, Int> {
 val alphabet = listOf('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 
 fun getCharAtIndex(index: Int): Char {
-    val reducedindex = index%26
-    return alphabet[reducedindex]
+    val reducedIndex = index%26
+    return alphabet[reducedIndex]
 }
 
-fun getIndexforChar(char: Char): Int {
+fun getIndexForChar(char: Char): Int {
     if (char in alphabet) return alphabet.indexOf(char)
     else return 0
 }
